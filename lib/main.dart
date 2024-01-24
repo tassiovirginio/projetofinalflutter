bcded39be348422d10345109396c8cd4a371ac8c
phdiamantino <phdiamantinoo@gmail.com>
2024-01-24 17:09:20 -0300

    ph config

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:projetofinalflutter/screens/my_home_screen.dart';
+import 'package:projetofinalflutter/screens/checkin.dart';
 import 'package:projetofinalflutter/utils/utils.dart';
+
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +16,15 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: color_background),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Sabores do Mundo'),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 232, 231, 229),
+          title: Text('Restaurante Sabores do Mundo', style: TextStyle (fontWeight: FontWeight.bold, fontSize: 22)),
+          elevation: 70,
+          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.vertical(bottom: Radius.circular(12))),
+          centerTitle: true,
+        ),
+      ),
     );
   }
 }
