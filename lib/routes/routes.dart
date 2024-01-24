49ba25eabd06c5eef67d4e16f44fdfa98d4eecf7
Tássio Virgínio <tassiovirginio@gmail.com>
2024-01-24 20:22:50 -0300

    alterando os sistema de rotas

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,18 +1,14 @@
 import 'package:projetofinalflutter/main.dart';
+import 'package:projetofinalflutter/screens/confirmacao_screen.dart';
 import 'package:projetofinalflutter/screens/finalizar_screen.dart';
 import 'package:projetofinalflutter/screens/login_screen.dart';
 import 'package:projetofinalflutter/screens/menu_screen.dart';
 
-enum Routes { LOGIN, MENU, FINAL }
-
-// class Routes {
-//   static const LOGIN = '/login';
-//   static const MENU = '/menu';
-//   static const FINAL = '/final';
-// }
+enum Routes { LOGIN, MENU, CONFIRMACAO, FINAL }
 
 var myroutes = {
   Routes.LOGIN.name: (context) => LoginScreen(title: title),
   Routes.MENU.name: (context) => MenuScreen(title: title),
+  Routes.CONFIRMACAO.name: (context) => ConfirmacaoScreen(title: title),
   Routes.FINAL.name: (context) => FinalizarScreen(title: title),
 };
