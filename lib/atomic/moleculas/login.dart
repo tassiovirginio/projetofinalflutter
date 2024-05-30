6a0da87094c251cd9f03ec025cb82438cd21d763
Tassio Virginio <tassiovirginio@gmail.com>
2024-05-30 20:17:06 -0300

    implementando

---
+++ lib/atomic/moleculas/login.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:crypto/crypto.dart';
+import 'package:get/get.dart';
+import 'package:projetofinalflutter/atomic/atomos/botao_login.dart';
+import 'package:projetofinalflutter/atomic/atomos/login_text_field.dart';
+import 'package:projetofinalflutter/atomic/atomos/senha_text_field.dart';
+import 'dart:convert';
+import 'package:projetofinalflutter/components/authentication_imput_decoration.dart';
+import 'package:projetofinalflutter/database/database.dart';
+import 'package:projetofinalflutter/screens/menu_screen.dart';
+import 'package:projetofinalflutter/utils/utils.dart';
+
+class LoginBox extends Container {
+  var loginTextField = LoginTextField();
+  var senhaTextField = SenhaTextField();
+
+  static List<String> usuarioLogado = [];
+
+  LoginBox(
+      {super.key,
+      required String title,
+      required this.loginTextField,
+      required this.senhaTextField})
+      : super(
+          margin: const EdgeInsets.all(10.0),
+          width: 100,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                child: loginTextField,
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                child: senhaTextField,
+              ),
+              Container(
+                  margin: const EdgeInsets.fromLTRB(100, 40, 100, 0),
+                  child: BotaoLogin(
+                      () => realizarLogin(senhaTextField, loginTextField, title))),
+            ],
+          ),
+        );
+}
+
+realizarLogin(var senhaTextField, var loginTextField, var title) {
+  String senhaMD5 =
+      md5.convert(utf8.encode(senhaTextField.controller!.text)).toString();
+  List<String> logado =
+      DataBase.fazerLogin(loginTextField.controller!.text, senhaMD5);
+  if (logado.isNotEmpty) {
+    LoginBox.usuarioLogado = logado;
+    Get.to(() => MenuScreen(title: title));
+  } else {
+    Get.snackbar("Erro", "Usuário ou senha inválidos");
+  }
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:crypto/crypto.dart';
 import 'package:get/get.dart';
 import 'package:projetofinalflutter/atomic/atomos/login_text_field.dart';
 import 'package:projetofinalflutter/atomic/atomos/senha_text_field.dart';
+import 'package:projetofinalflutter/atomic/moleculas/login.dart';
 import 'dart:convert';
 import 'package:projetofinalflutter/components/authentication_imput_decoration.dart';
 import 'package:projetofinalflutter/database/database.dart';
@@ -22,9 +23,6 @@ class LoginScreenState extends State<LoginScreen> {
   LoginScreenState(this.title);
 
   static List<String> usuarioLogado = [];
-
-  var loginTextField = LoginTextField();
-  var senhaTextField = SenhaTextField();
 
   @override
   Widget build(BuildContext) {
@@ -58,49 +56,11 @@ class LoginScreenState extends State<LoginScreen> {
                   color: colorFont),
             ),
             const SizedBox(height: 40),
-            Container(
-              margin: const EdgeInsets.all(10.0),
-              width: 100,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-                      child: loginTextField,
-                    ),
-                    Container(
-                      margin: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-                      child: senhaTextField,
-                    ),
-                    Container(
-                      margin: const EdgeInsets.fromLTRB(100, 40, 100, 0),
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: colorButton,
-                              shape: const RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(9.0)),
-                              )),
-                          onPressed: () {
-                            String senhaMD5 =
-                                md5.convert(utf8.encode(senhaTextField.controller!.text)).toString();
-                            List<String> logado =
-                                DataBase.fazerLogin(loginTextField.controller!.text, senhaMD5);
-                            if (logado.isNotEmpty) {
-                              usuarioLogado = logado;
-                              Get.to(() => MenuScreen(title: title));
-                            } else {
-                              Get.snackbar(
-                                  "Erro", "Usuário ou senha inválidos");
-                            }
-                          },
-                          child: const Text('Entrar',
-                              style: TextStyle(
-                                  fontSize: 20, color: colorButtonText))),
-                    ),
-                  ]),
-            ),
+            LoginBox(
+              title: title,
+              loginTextField: LoginTextField(),
+              senhaTextField: SenhaTextField(),
+            )
           ],
         ),
       ),
